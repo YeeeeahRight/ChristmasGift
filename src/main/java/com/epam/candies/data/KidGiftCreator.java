2a1239ea9c59478afa0f7738b61d880ed09f7776
YeeeeahRight <yeeeeeah.right@gmail.com>
2020-09-26 11:10:59 +0300

    Changed name of variables.

--- src/main/java/com/epam/candies/data/KidGiftCreator.java
+++ src/main/java/com/epam/candies/data/KidGiftCreator.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
 
 public class KidGiftCreator {
     public ArrayList<Candy> createGift() {
-        ArrayList<Candy> creator = new ArrayList<Candy>();
-        creator.add(new Candy("Truffle", 50));
-        creator.add(new Candy("Chocolate santa", 20.5));
-        creator.add(new Candy("Toffee", 70));
-        return creator;
+        ArrayList<Candy> candies = new ArrayList<Candy>();
+        candies.add(new Candy("Truffle", 50));
+        candies.add(new Candy("Chocolate santa", 20.5));
+        candies.add(new Candy("Toffee", 70));
+        return candies;
     }
 }
