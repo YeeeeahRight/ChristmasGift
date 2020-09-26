89b82307b03a3cd63fc90e29a7981f3f6e801d64
YeeeeahRight <yeeeeeah.right@gmail.com>
2020-09-26 14:26:58 +0300

    Created gift factory logic.

--- src/main/java/com/epam/candies/Main.java
+++ src/main/java/com/epam/candies/Main.java
@@ -1,23 +1,18 @@
 package com.epam.candies;
 
-import com.epam.candies.data.KidGiftCreator;
+import com.epam.candies.data.KidGiftFactory;
 import com.epam.candies.enums.PrinterType;
 import com.epam.candies.exceptions.NullGiftException;
 import com.epam.candies.exceptions.UnknownPrinterTypeException;
 import com.epam.candies.logic.CandyCalculator;
-import com.epam.candies.model.Candy;
 import com.epam.candies.model.KidGift;
 import com.epam.candies.view.GiftWeightPrinter;
 import com.epam.candies.view.GiftWeightPrinterFactory;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) throws UnknownPrinterTypeException, NullGiftException {
-        KidGiftCreator kidGiftCreator = new KidGiftCreator();
-        ArrayList<Candy> candies = kidGiftCreator.createGift();
-        KidGift kidGift = new KidGift();
-        kidGift.fillGift(candies);
+        KidGiftFactory kidGiftFactory = new KidGiftFactory();
+        KidGift kidGift = kidGiftFactory.createGift();
         CandyCalculator candyCalculator = new CandyCalculator();
         double giftWeight = candyCalculator.findGiftWeight(kidGift);
         GiftWeightPrinterFactory giftWeightPrinterCreator = new GiftWeightPrinterFactory();
--- src/main/java/com/epam/candies/data/KidGiftFactory.java
+++ src/main/java/com/epam/candies/data/KidGiftFactory.java
@@ -1,15 +1,18 @@
 package com.epam.candies.data;
 
 import com.epam.candies.model.Candy;
+import com.epam.candies.model.KidGift;
 
 import java.util.ArrayList;
 
-public class KidGiftCreator {
-    public ArrayList<Candy> createGift() {
+public class KidGiftFactory {
+    public KidGift createGift() {
         ArrayList<Candy> candies = new ArrayList<Candy>();
         candies.add(new Candy("Truffle", 50));
         candies.add(new Candy("Chocolate santa", 20.5));
         candies.add(new Candy("Toffee", 70));
-        return candies;
+        KidGift gift = new KidGift();
+        gift.fillGift(candies);
+        return gift;
     }
 }
